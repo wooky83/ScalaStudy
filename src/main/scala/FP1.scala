7dec963521d1d0ba63d1be1c3a3cd90c7017ec29
권성욱(Sungwook Kwon)/OCB개발팀/SKP <baw0803@sk.com>
2017-04-19 16:14:10 +0900

    [Scala] Test

--- src/main/scala/FP1.scala
+++ src/main/scala/FP1.scala
@@ -3,4 +3,12 @@
   */
 class FP1 {
 
+  def factorial(i : Int) : Long = {
+    def fact(i:Int, accumulator : Int) : Long = {
+      if (i <= 1) accumulator
+      else fact(i-1, i*accumulator)
+      }
+      fact(i, 1)
+  }
+  (0 to 5) foreach(i => println(factorial(i)))
 }
