d23621d75df6ae5688005f7b312c8aa7e78a451f
권성욱(Sungwook Kwon)/OCB개발팀/SKP <baw0803@sk.com>
2017-04-19 16:38:09 +0900

    [Scala] @tailrec Study

--- src/main/scala/FP1.scala
+++ src/main/scala/FP1.scala
@@ -1,10 +1,13 @@
+import scala.annotation.tailrec
+
 /**
   * Created by 1002659 on 2017. 4. 19..
   */
 class FP1 {
 
-  def factorial(i : Int) : Long = {
-    def fact(i:Int, accumulator : Int) : Long = {
+  def factorial(i : BigInt) : BigInt = {
+    @tailrec
+    def fact(i:BigInt, accumulator : BigInt) : BigInt = {
       if (i <= 1) accumulator
       else fact(i-1, i*accumulator)
       }
