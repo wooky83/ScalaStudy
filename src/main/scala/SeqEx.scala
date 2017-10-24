2919962fa527f856a3411779e385a5ccee43f165
1002659 <baw0803@sk.com>
2017-10-24 17:17:39 +0900

    remove

--- src/main/scala/SeqEx.scala
+++
@@ -1,9 +0,0 @@
-/**
-  * Created by 1002659 on 2017. 4. 24..
-  */
-import scala.math.BigInt
-
-class SeqEx {
-
-  val fibs: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map( n => n._1 + n._2)
-}
